efb55ae0b99da863937a5229b1345366def10164
CheHedley <42931018+CheHedley@users.noreply.github.com>
2018-09-13 16:03:12 +0100

    Add files via upload

---
+++ Songs/Song.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Songs
+{
+
+
+    public class Song
+    {
+        private string name;
+        private string artist;
+        private int copiesSold;
+
+        public Song(string name, string artist, int copiesSold)
+        {
+            this.name = name;
+            this.artist = artist;
+            this.copiesSold = copiesSold;
+            GetCertification(); 
+        }
+
+
+        public string GetArtist()
+        {
+            return artist;
+        }
+        public string GetDetails()
+        {            
+            string details = $"Name: {name} Artist: {artist} Copies Sold: {copiesSold}";
+            return details;
+        }
+        public string GetCertification()
+        {
+            if (copiesSold < 200000)
+            {
+                return null;
+            }
+            else if (copiesSold >= 200000 && copiesSold < 400000)
+            {
+                return "Silver";
+            }
+            else if (copiesSold >= 400000 && copiesSold < 600000)
+            {
+                return "Gold";
+            }
+            else if (copiesSold >= 600000)
+            {
+                return "Platinum";
+            }
+            else
+            {
+                return string.Empty;
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Songs;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Song s;
+        Song s2;
+        Song s3;
+        Song s4;
+        [TestInitialize]
+        public void InitialiseSongTest()
+        {
+            s = new Song("Dear Mama", "Tupac", 1400000);
+            s2 = new Song("Changes", "Tupac", 580000);
+            s3 = new Song("Ghetto Gospel", "Tupac", 390000);
+            s4 = new Song("Smile", "Tupac", 190000);
+
+        }
+        [TestMethod]
+        public void TestDetails()
+        {
+            string realDetailsS = s.GetDetails();
+            Assert.AreEqual("Name: Dear Mama Artist: Tupac Copies Sold: 1400000", realDetailsS, "", "The details of the track do not match.");
+            string realDetailsS2 = s2.GetDetails();
+            Assert.AreEqual("Name: Changes Artist: Tupac Copies Sold: 580000", realDetailsS2, "", "The details of the track do not match.");
+
+        }
+        [TestMethod]
+        public void TestCertification()
+        {
+            string realCertification = s.GetCertification();
+            Assert.AreEqual("Platinum", realCertification, "", "The certification is incorrect");
+        }
+
+    }
+}
